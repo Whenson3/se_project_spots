ad2144c1d353af2aa8b3f1b22e91efff5626c0c7
William Henson <Whenson3@yahoo.com>
2025-08-22 10:05:47 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,15 @@ const editProfileForm = editProfileModal.querySelector(".modal__form");
 const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
 const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 const addCardFormElement = newPostModal.querySelector(".modal__form");
-const nameInput = newPostModal.querySelector("#card-caption-input");
-const linkInput = newPostModal.querySelector("#card-image-input");
+const captionInput = newPostModal.querySelector("#card-caption-input");
+const imageUrlInput = newPostModal.querySelector("#card-image-input");
 
 
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
-const CaptionEl = document.querySelector(".card__title");
+const captionEl = document.querySelector(".card__title");
 const imageLinkEl = document.querySelector(".card__image");
 
 editProfileBtn.addEventListener("click", function () {
@@ -47,11 +47,10 @@ editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
-  console.log("nameInput");
-  console.log("linkInput");
+  console.log(nameInput.value)
   newPostModal.classList.remove("modal_is-opened");
 }
 
-// Create the submit listener.
-addCardFormElement.addEventListener('submit', handleAddCardSubmit);
+
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
 
